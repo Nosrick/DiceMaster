c7c0a338f7bbcd3e603aaa123166126c8cc548e4
nosrick <nosrick@gmail.com>
2017-12-16 21:31:29 +0000

    Actual commit

---
+++ Assets/Code/Touch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Touch
+{
+    public Vector3 Start
+    {
+        get;
+        set;
+    }
+
+    public Vector3 End
+    {
+        get;
+        set;
+    }
+}
